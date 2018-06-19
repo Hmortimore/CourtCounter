0bdc9b593d9b94c81da1b0bddb2ffde424ecbbf7
Hmortimore <Hmortimore@gmail.com>
2018-06-19 17:44:43 -0400

    Cleaning the java code as per Udacity reviewer recommendations

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,76 +7,96 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+    int scoreTeamA = scoreTeamA;
+    int scoreTeamB = scoreTeamB;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+        displayForTeamA(0);
     }
     /**
-     * Displays the given score for Team A.
+     * Displays the given score for Team A
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    public void addThreePointsForTeamA(View v) {
-        displayForTeamA(3);
-        scoreTeamA = scoreTeamA + 3;
+    public void touchdownForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 6;
         displayForTeamA(scoreTeamA);
     }
 
-    public void addTwoPointsForTeamA(View v) {
+    public void fieldGoalForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void Conversion2ForTeamA(View v) {
         displayForTeamA(2);
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-    public void addOnePointForTeamA(View v) {
+    public void Conversion1ForTeamA(View v) {
         displayForTeamA(1);
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
+    public void safetyForTeamA(View v) {
+        displayForTeamA(2);
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
     /**
-     * Displays the given score for Team B.
+     * Displays the given score for Team B
      */
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    public void addThreePointsForTeamB(View v) {
-        displayForTeamB(3);
-        scoreTeamB = scoreTeamB + 3;
+    public void touchdownForTeamB(View v) {
+        displayForTeamB(6);
+        scoreTeamB = scoreTeamB + 6;
         displayForTeamB(scoreTeamB);
     }
 
-    public void addTwoPointsForTeamB(View v) {
+    public void fieldGoalForTeamB(View v) {
         displayForTeamB(2);
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
-    public void addOnePointForTeamB(View v) {
+    public void Conversion2ForTeamB(View v) {
+        displayForTeamB(2);
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void Conversion1ForTeamB(View v) {
         displayForTeamB(1);
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
 
+    public void safetyForTeamB(View v) {
+        displayForTeamB(2);
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
     /**
      * This will reset score for Team A & Team B
      */
     public void resetScore(View v) {
         scoreTeamA = 0;
-        displayForTeamA(0);
+        displayForTeamA(scoreTeamA);
         scoreTeamB = 0;
-        displayForTeamB(0);
+        displayForTeamB(scoreTeamB);
     }
 
 
